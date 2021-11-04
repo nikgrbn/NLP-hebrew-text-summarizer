1044c8ee361718a0212409914fa6c04d0b4e96ba
Nikita Grebenchuk <rmaps123@yandex.ru>
2021-11-04 19:02:26 +0200

    feat: separated content to main file

--- src/tf_idf.py
+++ src/tf_idf.py
@@ -51,13 +51,3 @@ def count_word_from_sentences(word, sentences):
             count += 1
 
     return count
-
-
-test_text = """בתחקיר שפורסם בכאן חדשות סיפר א', שנפגש עם אוחובסקי לפני כשנה וחצי, כי השניים קבעו להיפגש לאחר ששוחחו באפליקציית היכרויות. אוחובסקי, כך אמר, השתמש בשם בדוי אך שלח את תמונותיו האמיתיות. "הוא תקשר כמו חיה", סיפר א'. "יותר מחמש פעמים הוא ניסה להפוך אותי בכוח ולגעת". הוא ציין כי ביקש ממנו להפסיק באופן נחרץ, ולפי הפרסום גם עבר בדיקת פוליגרף שבה נמצא דובר אמת."""
-
-print(create_sentence_array(test_text))
-print("\n\nIDF TABLE: \n")
-[print(key, ': %.3f' % value) for key, value in create_idf_table(test_text).items()]
-
-print("\n\nTF TABLE: \n")
-[print(key, value) for key, value in create_tf_table(test_text).items()]
