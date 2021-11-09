72761138ccb62c7036cfba6bbbff0eb630c830e4
RustyMed <ShayMedioni21@gmail.com>
2021-11-09 23:55:13 +0200

    test: check the svd table

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
+from src.svd import *
 from src.tf_idf import *
+
 
 
 def main():
     test_text = """בתחקיר שפורסם בכאן חדשות סיפר א', שנפגש עם אוחובסקי לפני כשנה וחצי, כי השניים קבעו להיפגש לאחר ששוחחו באפליקציית היכרויות. אוחובסקי, כך אמר, השתמש בשם בדוי אך שלח את תמונותיו האמיתיות. "הוא תקשר כמו חיה", סיפר א'. "יותר מחמש פעמים הוא ניסה להפוך אותי בכוח ולגעת". הוא ציין כי ביקש ממנו להפסיק באופן נחרץ, ולפי הפרסום גם עבר בדיקת פוליגרף שבה נמצא דובר אמת."""
 
+    tf = create_tf_table(test_text)
+    idf = create_idf_table(test_text)
+    svd = create_svd_table(tf, idf)
+
+    print("\n\nSVD TABLE: \n")
+    [print(key, value) for key, value in svd.items()]
+
     print("\n\nIDF TABLE: \n")
-    [print(key, ': %.3f' % value) for key, value in create_idf_table(test_text).items()]
+    [print(key, ': %.3f' % value) for key, value in idf.items()]
 
     print("\n\nTF TABLE: \n")
-    [print(key, value) for key, value in create_tf_table(test_text).items()]
+    [print(key, value) for key, value in tf.items()]
 
 
 if __name__ == "__main__":
