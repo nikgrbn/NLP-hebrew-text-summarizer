d7f7ec316ce79e840e055d26f48a90eac78c10a9
Rmaps <Rmaps123@yandex.ru>
2022-02-19 12:34:45 +0200

    feat: key sentances ordered accrodingly to text

--- src/main.py
+++ src/main.py
@@ -23,8 +23,16 @@ def main():
 
     time.sleep(0.1)
     print("Keywords:{}\n".format(key_words))
-    print("Key-sentences:")
-    print(*key_sentences, sep='\n')
+    print_key_sentences_orderly(test_text, key_sentences)
+    # print("Key-sentences:")
+    # print(*key_sentences, sep='\n')
+
+
+def print_key_sentences_orderly(text, key_sentences):
+    text_sentences = tokenize.sent_tokenize(text)
+    for sent in text_sentences:
+        if sent in key_sentences:
+            print(sent)
 
 
 def print_svd(svd):
